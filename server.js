1e3558277d443b18a9938f4f90423fdab9570bd3
John Slota <john.slota@gmail.com>
2016-04-20 14:53:00 -0600

    sending messages to every client implemented

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app)
+                 .listen(port, function () {
+                    console.log('Listening on port ' + port + '.');
+                  });
+
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  io.sockets.emit('usersConnected', io.engine.clientsCount);
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+  });
+});
+
+module.exports = server;
