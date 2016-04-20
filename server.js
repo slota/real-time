5a4de2da30aef7d7f1576f2ad90be06ce2afefaa
John Slota <john.slota@gmail.com>
2016-04-20 16:45:53 -0600

    sending messages from the client to the server

--- server.js
+++ server.js
@@ -3,21 +3,24 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app)
+.listen(port, function () {
+  console.log('Listening on port ' + port + '.');
+});
+
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
+var votes = {};
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
-const port = process.env.PORT || 3000;
-
-const server = http.createServer(app)
-                 .listen(port, function () {
-                    console.log('Listening on port ' + port + '.');
-                  });
-
-const socketIo = require('socket.io');
-const io = socketIo(server);
 
 io.on('connection', function (socket) {
   console.log('A user has connected.', io.engine.clientsCount);
@@ -29,6 +32,25 @@ io.on('connection', function (socket) {
   socket.on('disconnect', function () {
     console.log('A user has disconnected.', io.engine.clientsCount);
   });
+
+  socket.on('message', function (channel, message) {
+    console.log(channel, message);
+  });
+
+  socket.on('message', function (channel, message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      console.log(votes);
+    }
+  });
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    console.log(votes);
+    io.sockets.emit('usersConnected', io.engine.clientsCount);
+  });
 });
+
 
 module.exports = server;
