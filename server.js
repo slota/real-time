58f76f4c702083ef10161131fcc4265e6fd669eb
John Slota <john.slota@gmail.com>
2016-04-21 10:33:38 -0600

    installed mocha and express

--- server.js
+++ server.js
@@ -66,4 +66,10 @@ function countVotes(votes) {
   return voteCount;
 }
 
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
+
 module.exports = server;
