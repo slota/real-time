580e6dc8ab3df0c872cb5d8286ae96683574d567
John Slota <john.slota@gmail.com>
2016-04-23 20:34:49 -0600

    debugger removed

--- public/client.js
+++ public/client.js
@@ -8,31 +8,23 @@ var voteMessage = document.getElementById('vote-message');
 var haventVoted = true;
 
 socket.on('usersConnected', function (count) {
-  debugger
   connectionCount.innerText = 'Connected Users: ' + count;
 });
 
 socket.on('statusMessage', function (message) {
-  debugger
   statusMessage.innerText = message;
 });
 
 socket.on('displayCount', function (message) {
-  debugger
   currentTally.innerText = JSON.stringify(message, null, 4);
 
 });
 
 socket.on('voteMessage', function (message) {
-
-  debugger
   voteMessage.innerText = message;
 });
 
 socket.on('voteCount', function (votes) {
-  debugger
-
-
   currentTally.innerText = JSON.stringify(votes, null, 4);
   console.log(votes);
 });
@@ -41,14 +33,10 @@ for (var i = 0; i < buttons.length; i++) {
 
 
   buttons[i].addEventListener('click', function () {
-
-    console.log("jesus christ rockes")
     if (haventVoted) {
-      debugger
       haventVoted = false
       socket.send('voteCast', this.innerText);
     } else {
-      debugger
       voteMessage.innerText = "You've already voted, sorry :("
     }
   });
