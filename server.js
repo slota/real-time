92c33fc7d24eb6964e8265a81571f05ff7f3e7bd
John Slota <john.slota@gmail.com>
2016-04-23 20:03:16 -0600

    refactored

--- server.js
+++ server.js
@@ -49,23 +49,12 @@ app.post('/poll', function (req, res){
 });
 
 app.get('/polls/:id', function (req, res){
-  console.log("polls id")
   res.render('poll', { data: app.locals.poll })
 } )
 
 app.get('/admin-view/:id', function (req, res){
-  console.log("get admin view")
   res.render('admin-view', { data: app.locals.poll })
 } )
-
-// app.get('/', function(req, response){
-//   response.send('hello world');
-//   console.log("IS THIS WORKING!?!")
-// });
-
-// app.get('/', (request, response) => {
-// });
-
 
 io.on('connection', function (socket) {
 
@@ -77,11 +66,8 @@ io.on('connection', function (socket) {
   socket.on('message', function (channel, message) {
     if (channel === 'voteCast') {
       votes[socket.id] = message;
-      console.log("before countVotes")
-      console.log(votes[socket.id])
       countVotes(votes);
       votes = {}
-      console.log("after countvotes")
       socket.emit('voteMessage', "You voted for " + message);
       io.sockets.emit('displayCount', app.locals.poll["voteCount"]);
     }
@@ -90,7 +76,6 @@ io.on('connection', function (socket) {
   console.log("sockets")
   console.log(app.locals.poll["voteCount"])
   io.sockets.emit('displayCount', app.locals.poll["voteCount"]);
-
 
   socket.on('disconnect', function () {
     console.log('A user has disconnected.', io.engine.clientsCount);
@@ -101,32 +86,15 @@ io.on('connection', function (socket) {
   socket.on('disconnect', function () {
     console.log('A user has disconnected.', io.engine.clientsCount);
     delete votes[socket.id];
-    console.log(votes);
     io.sockets.emit('usersConnected', io.engine.clientsCount);
   });
 });
 
 function countVotes(votes) {
-  // console.log(votes.value)
-  // console.log(app.locals.poll.voteCount)
-  // console.log(!(votes.value in app.locals.poll.voteCount))
-  // if(!(votes.value in app.locals.poll.voteCount)){
-  //   app.locals.poll.voteCount[votes.value] = 0
-  // }
-  // //   app.locals.poll.voteCount.votes.value = 0
-  // // }
-  console.log( "stage 2")
-  console.log(votes)
   for (var vote in votes) {
     app.locals.poll.voteCount[votes[vote]]++
   }
   return app.locals.poll.voteCount;
 }
-
-// if (!module.parent) {
-//   app.listen(app.get('port'), () => {
-//     console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-//   });
-// }
 
 module.exports = app;
